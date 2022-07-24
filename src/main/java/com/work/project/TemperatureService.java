448874587658f2b1f56f937673cd3da06f351f51
utgard <daniel.strbak@seznam.cz>
2022-07-24 22:00:59 +0200

    Add functionality to list every item to service

--- src/main/java/com/work/project/TemperatureService.java
+++ src/main/java/com/work/project/TemperatureService.java
@@ -3,12 +3,19 @@ package com.work.project;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class TemperatureService {
     TemperatureRepository temperatureRepository;
 
+    public List<Temperature> getTemperatures() {
+        return temperatureRepository.findAll();
+    }
+
     public void addTemperature(Temperature temperature) {
         temperatureRepository.save(temperature);
     }
+
 }
